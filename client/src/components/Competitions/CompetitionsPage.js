50b96d094ae94f2ee88bd0d1b8d3a5cad2871c1e
federicomazzini3 <federico.mazzini3@studio.unibo.it>
2022-12-01 17:22:43 +0100

    move search in a separate component

--- client/src/components/Competitions/CompetitionsPage.js
+++ client/src/components/Competitions/CompetitionsPage.js
@@ -7,6 +7,7 @@ import { borderRadius, Container, spacing } from '@mui/system';
 import Pagination from '../Pagination/Pagination'
 import { Paper, AppBar, TextField, Button, Chip, Grid, Box } from '@mui/material';
 import { useHistory, useLocation } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -16,43 +17,12 @@ const CompetitionsPage = () => {
 
     const dispatch = useDispatch();
     const query = useQuery();
-    const history = useHistory();
     const page = query.get('page') || 1;
-    const searchQuery = query.get('searchQuery');
-    const [search, setSearch] = useState('')
-
-    const searchCompetitions = () => {
-        if (search.trim()) {
-            dispatch(getCompetitionsBySearch(search, 1))
-            history.push(`/competitions/search?searchQuery=${search || 'none' }&page=1`);
-        } else {
-            history.push('/competitions')
-        }
-    }
-
-    const onEnterButton = (e) => {
-        if (e.keyCode === 13) {
-            searchCompetitions();
-        }
-    }
 
     return (
         <Container sx={{ py: 8 }} maxWidth="md">
-            <AppBar position='static' color='inherit' sx={{ mb: 2, borderRadius: 5, p: 2, pt: 1 }}>
-                <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={10}>
-                        <TextField name='search' variant='standard' label='   Search...' fullWidth value={search} onChange={(e) => { setSearch(e.target.value) }} onKeyDown={onEnterButton}/>
-                    </Grid>
-                    <Grid item xs={12} sm={2}>
-                        <Button onClick={searchCompetitions} variant='contained' color='primary' sx={{ mt: 2 }}>Search</Button>
-                    </Grid>
-                </Grid>
-                </Box>
-            </AppBar>
-
+            <SearchBar></SearchBar>
             <Competitions></Competitions>
-
             <Paper elevation={6} sx={{ borderRadius: 4, mt: 2, p: 2 }}>
                 <Pagination  page={page}/>
             </Paper>
---
+++ client/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { getCompetitionsBySearch } from '../../actions/competitions';
+import { Paper, AppBar, TextField, Button, Chip, Grid, Box } from '@mui/material';
+import { useHistory, useLocation } from 'react-router-dom';
+
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
+const SearchBar = () => {
+
+
+    const dispatch = useDispatch();
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const [search, setSearch] = useState('')
+
+    const searchCompetitions = () => {
+        if (search.trim()) {
+            dispatch(getCompetitionsBySearch(search, 1))
+            history.push(`/competitions/search?searchQuery=${search || 'none'}&page=1`);
+        } else {
+            history.push('/competitions')
+        }
+    }
+
+    const onEnterButton = (e) => {
+        if (e.keyCode === 13) {
+            searchCompetitions();
+        }
+    }
+    return (
+        <AppBar position='static' color='inherit' sx={{ mb: 2, borderRadius: 5, p: 2, pt: 1 }}>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={10}>
+                        <TextField name='search' variant='standard' label='   Search...' fullWidth value={search} onChange={(e) => { setSearch(e.target.value) }} onKeyDown={onEnterButton} />
+                    </Grid>
+                    <Grid item xs={12} sm={2}>
+                        <Button onClick={searchCompetitions} variant='contained' color='primary' sx={{ mt: 2 }}>Search</Button>
+                    </Grid>
+                </Grid>
+            </Box>
+        </AppBar>
+    )
+}
+
+export default SearchBar
