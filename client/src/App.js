c67ad6d222bc56d9a4769be1a31279748d4455c1
CarloDiRaddo <carlo.diraddo@gmail.com>
2022-12-13 17:24:23 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 import Footer from "./components/Footer/Footer";
 import Form from "./components/Form/Form";
-import User from "./components/User/User";
+import User from "./components/User/user";
 import { useSelector } from "react-redux";
 import CompetitionsPage from "./components/CompetitionsPage/CompetitionsPage";
 import CompetitionPage from "./components/CompetitionPage/CompetitionPage"
